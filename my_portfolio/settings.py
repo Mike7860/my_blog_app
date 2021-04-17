7c0dac46c996eb95168cf11c2c1524f9b67ffe29
Mike7860 <michal.78.60@gmail.com>
2021-04-17 18:50:21 +0200

    changed db in settings.py

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +125,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+django_heroku.settings(locals())
